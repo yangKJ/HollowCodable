80b76a576f4cc540a50b4ea2200363135d5f1d30
Condy <yangkj310@gmail.com>
2024-07-07 13:07:37 +0800

    Add the loss value property wrapper

---
+++ Sources/Codings/LossyArrayCoding.swift
@@ -0,0 +1,42 @@
+//
+//  LossyArrayCoding.swift
+//  CodableExample
+//
+//  Created by Condy on 2024/7/7.
+//
+
+import Foundation
+
+/// Decodes Arrays filtering invalid values if applicable
+/// `@LossyArrayCoding` decodes Arrays and filters invalid values if the Decoder is unable to decode the value.
+/// This is useful if the Array is intended to contain non-optional types.
+public struct LossyArrayValue<T: Codable>: Transformer {
+    
+    let value: [T]
+    
+    public typealias DecodeType = [T]
+    public typealias EncodeType = [T]
+    
+    public init?(value: Any) {
+        guard let decoder = value as? Decoder, var container = try? decoder.unkeyedContainer() else {
+            return nil
+        }
+        var elements: [T] = []
+        while !container.isAtEnd {
+            do {
+                let value = try container.decode(T.self)
+                elements.append(value)
+            } catch {
+                _ = try? container.decode(JSONValue.self)
+            }
+        }
+        self.value = elements
+    }
+    
+    public func transform() throws -> [T]? {
+        value
+    }
+}
+
+extension LossyArrayValue: Equatable where T: Equatable { }
+extension LossyArrayValue: Hashable where T: Hashable { }
