d802feea4c97799f3da8a12969659ebd5dc4b492
Condy <yangkj310@gmail.com>
2024-07-29 15:56:56 +0800

    Add two method for mapping

--- HollowCodable.podspec
+++ HollowCodable.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HollowCodable'
-  s.version          = '2.1.4'
+  s.version          = '2.1.5'
   s.summary          = 'Codable Customization using Property Wrappers.'
   
   # This description is used to generate tags and improve search results.
--- Sources/HollowCodable.swift
+++ Sources/HollowCodable.swift
@@ -25,11 +25,56 @@ extension HollowCodable {
     public static func mapping(mapper: HelpingMapper) { }
     
     public mutating func didFinishMapping() { }
+}
+
+extension HollowCodable {
     
-    func mutating(_ block: (inout Self) -> Void) -> Self {
+    public func mutating(_ block: (inout Self) -> Void) -> Self {
         var options = self
         block(&options)
         return options
+    }
+    
+    /// 映射非空数据，字段数据为空则取后者`infomation`对应字段数据
+    /// Map the not nil data.
+    /// - Parameter infomation: Data source to be mapped.
+    /// - Returns: Mapped data.
+    public func mappingNotNil<T: HollowCodable>(with infomation: T?) -> T {
+        guard let infomation = infomation else {
+            return self as! T
+        }
+        let dict_ = Dictionary(Mirror(reflecting: infomation).children.map { ($0.label, $0.value) }) { $1 }
+        var dict: [String: Any] = [:]
+        for property in Mirror(reflecting: self).children {
+            guard let key = property.label else {
+                continue
+            }
+            if case Optional<Any>.some(let value) = property.value { //非空判断
+                dict[key] = value
+            } else {
+                dict[key] = dict_[key]
+            }
+        }
+        let result = T.deserialize(from: dict)
+        return result ?? (self as! T)
+    }
+    
+    /// 不同对象映射后者非空数据
+    /// Different objects map the latter's non-empty data.
+    /// - Parameters:
+    ///   - type: self corresponding type class.
+    ///   - infomation: Data source to be mapped.
+    /// - Returns: Mapped data.
+    public func mappingLatterNotNil<T: HollowCodable, U: HollowCodable>(type: U.Type, latter infomation: T?) -> U {
+        guard let infoDict = infomation?.toJSON() else {
+            return self as! U
+        }
+        let maps = self.toJSON()?.compactMap { key, value in
+            (key, infoDict[key] ?? value)
+        } ?? []
+        let dict = Dictionary.init(maps, uniquingKeysWith: { _, old in old })
+        let result = U.deserialize(from: dict)
+        return result ?? (self as! U)
     }
 }
 
